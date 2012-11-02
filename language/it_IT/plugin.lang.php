c1a1cacdc9e61e3bac9d13ca046dced2bd652ea4
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-11-02 23:19:04 +0000

    [user_mass_register] Add it_IT, thanks to : virgigiole
    git-svn-id: http://piwigo.org/svn/extensions/user_mass_register@18899 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/it_IT/plugin.lang.php
@@ -0,0 +1,31 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['User Mass Register'] = 'Registrazione utenti in blocco';
+$lang['%d registrations on error: %s'] = '%d registrazioni su errore: %s';
+$lang['Register users'] = 'Utenti registrati';
+$lang['Email addresses'] = 'Indirizzi email';
+$lang['(one on each line)'] = '(uno su ogni riga)';
+$lang['%d users registered'] = '%d utenti registrati';
+$lang['%d email addresses rejected: %s'] = '%d indirizzo email rifiutato: %s';
+$lang['%d email addresses already exist: %s'] = '%d indirizzo email già presente: %s';
+?>
