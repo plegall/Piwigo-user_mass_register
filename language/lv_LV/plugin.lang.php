ab90b11c4c315fea3fcdbe82579241a6e357d8e0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-11-02 08:20:52 +0000

    [user_mass_register] Add lv_LV, thanks to : agrisans
    git-svn-id: http://piwigo.org/svn/extensions/user_mass_register@18886 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/lv_LV/plugin.lang.php
@@ -0,0 +1,31 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d registrations on error: %s'] = '%d kļūdaina reģistrācija: %s';
+$lang['User Mass Register'] = 'Lietotāju Masveida reģistrācija';
+$lang['Register users'] = 'Reģistrēt lietotājus';
+$lang['%d users registered'] = '%d reģistrēti lietotāji';
+$lang['Email addresses'] = 'E-pasta adreses';
+$lang['(one on each line)'] = '(pa vienam katrā rindā)';
+$lang['%d email addresses rejected: %s'] = '%d e-pasta adreses atteiktas: %s';
+$lang['%d email addresses already exist: %s'] = '%d e-pasta adreses jau eksistē: %s ';
+?>
