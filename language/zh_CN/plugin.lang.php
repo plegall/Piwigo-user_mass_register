9d712f4bdf323763a5785d6783fe98b6095401a8
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-11-03 18:47:32 +0000

    [user_mass_register] Add zh_CN, thanks to : winson
    git-svn-id: http://piwigo.org/svn/extensions/user_mass_register@18909 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,31 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d email addresses already exist: %s'] = '%d 邮件地址已存在：%s';
+$lang['%d email addresses rejected: %s'] = '%d 邮件地址被拒绝：%s';
+$lang['%d registrations on error: %s'] = '%d 注册出现错误：%s';
+$lang['%d users registered'] = '%d 用户注册';
+$lang['(one on each line)'] = '(每行一个)';
+$lang['Email addresses'] = '邮件地址';
+$lang['Register users'] = '注册用户';
+$lang['User Mass Register'] = '用户批量注册';
+?>
