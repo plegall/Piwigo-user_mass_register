d3dcd6cc1d1d2177517be606f700152bd1338161
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-11-07 05:59:46 +0000

    [user_mass_register] Add ru_RU, thanks to : rocket
    git-svn-id: http://piwigo.org/svn/extensions/user_mass_register@18964 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ru_RU/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d email addresses already exist: %s'] = '%d Email уже существует: %s';
+$lang['%d email addresses rejected: %s'] = '%d Email адрес отклонен: %s';
+$lang['%d users registered'] = '%d пользователи зарегистрированы';
+$lang['(one on each line)'] = '(по одному в строку)';
+$lang['Email addresses'] = 'Адреса Email';
+$lang['Register users'] = 'Регистрация пользователей';
+$lang['User Mass Register'] = 'Массовая регистрация пользователей';
+?>
