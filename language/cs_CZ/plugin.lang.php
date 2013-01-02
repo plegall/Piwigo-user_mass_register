66e093697a1a178709db61758b601b276f99b240
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-02 13:30:43 +0000

    [user_mass_register] Add cs_CZ, thanks to : ZdenekMaterna
    git-svn-id: http://piwigo.org/svn/extensions/user_mass_register@19721 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d email addresses already exist: %s'] = '%d e-mailová adresa už je registrována: %s';
+$lang['%d email addresses rejected: %s'] = '%d e-mailová adresa zamítnuta: %s';
+$lang['%d registrations on error: %s'] = '%d registrací s chybou: %s';
+$lang['%d users registered'] = '%d registrovaných uživatelů';
+$lang['(one on each line)'] = '(jeden na řádek)';
+$lang['Email addresses'] = 'E-mailová adresa';
+$lang['Example'] = 'Příklad';
+$lang['Register users'] = 'Registrovat uživatele';
+$lang['User Mass Register'] = 'Hromadná registrace uživatelů';
+?>
