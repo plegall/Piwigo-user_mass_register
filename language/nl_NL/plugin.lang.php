2ea84d63e1282d608c6edcb7655f9e4a9ae1dccd
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-02-09 07:07:05 +0000

    [user_mass_register] Add nl_NL, thanks to : dirk
    git-svn-id: http://piwigo.org/svn/extensions/user_mass_register@20623 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d users registered'] = '%d geregistreerde gebruikers';
+$lang['%d registrations on error: %s'] = '%d bij registraties een fout: %s';
+$lang['%d email addresses rejected: %s'] = '%d e-mailadressen afgewezen: %s';
+$lang['%d email addresses already exist: %s'] = '%d e-mailadressen bestaan ​​al: %s';
+$lang['User Mass Register'] = 'Gebruikers Massa Registratie';
+$lang['(one on each line)'] = '(een per regel)';
+$lang['Email addresses'] = 'E-mail adressen';
+$lang['Example'] = 'Voorbeeld';
+$lang['Register users'] = 'Geregistreerde gebruikers';
+?>
