b2ac60049942275ef7eb44b4b334390d2217c7c6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-05-17 04:31:14 +0000

    [user_mass_register] Add uk_UA, thanks to : marchelly
    git-svn-id: http://piwigo.org/svn/extensions/user_mass_register@22712 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/uk_UA/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d email addresses already exist: %s'] = '%d email адрес вже існує: %s';
+$lang['%d email addresses rejected: %s'] = '%d email адрес відхилено: %s';
+$lang['%d registrations on error: %s'] = '%d реєстрацій на помилці: %s';
+$lang['%d users registered'] = '%d користувачів зареєстровано';
+$lang['(one on each line)'] = '(по одному на кожний рядок)';
+$lang['Email addresses'] = 'Адреси email';
+$lang['Example'] = 'Приклад';
+$lang['Register users'] = 'Зареєструвати користувачів';
+$lang['User Mass Register'] = 'Кількісна реєстрація користувачів';
+?>
