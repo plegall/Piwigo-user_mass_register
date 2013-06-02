131227c5bb01d2311ad8c17fe5a5663b2b4f57d4
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-06-02 19:38:29 +0000

    [user_mass_register] Add ca_ES, thanks to : AlbertParera
    git-svn-id: http://piwigo.org/svn/extensions/user_mass_register@23018 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ca_ES/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d email addresses already exist: %s'] = '%d l\'adreça de correu electrònic ja existeix: %s';
+$lang['%d email addresses rejected: %s'] = '%d adreça de correu electrònic rebutjada: %s';
+$lang['%d registrations on error: %s'] = '%d registres amb error: %s';
+$lang['%d users registered'] = '%d usuaris registrats.';
+$lang['(one on each line)'] = '(un en cada línia)';
+$lang['Email addresses'] = 'Adreça de correu electrònic';
+$lang['Example'] = 'Exemple';
+$lang['Register users'] = 'Registrar usuaris';
+$lang['User Mass Register'] = 'Registre massiu d\'usuaris';
+?>
