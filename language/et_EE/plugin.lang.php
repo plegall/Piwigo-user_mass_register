7f0a65c94a41921d2fbdb86dd4caa56c613c939d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-09-02 18:50:48 +0000

    [user_mass_register] Add et_EE, thanks to : Eggy
    git-svn-id: http://piwigo.org/svn/extensions/user_mass_register@24317 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/et_EE/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d email addresses already exist: %s'] = '%d e-mail\'i aadress on juba olemas: %s';
+$lang['%d email addresses rejected: %s'] = '%d e-mail\'i aadress ei sobi: %s';
+$lang['%d registrations on error: %s'] = '%d registreerimise viga: %s';
+$lang['%d users registered'] = '%d kasutaja registreeritud: %s';
+$lang['(one on each line)'] = '(üks igale reale)';
+$lang['Email addresses'] = 'E-maili aadress';
+$lang['Example'] = 'Näide';
+$lang['Register users'] = 'Registreeri kasutajad';
+$lang['User Mass Register'] = 'Kasuta massi registreerimist';
+?>
