baa67938e9312ed08cc4fd237309e91d4483c7b5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-10-20 20:23:59 +0000

    [user_mass_register] Add sv_SE, thanks to : kabo
    git-svn-id: http://piwigo.org/svn/extensions/user_mass_register@25043 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sv_SE/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['User Mass Register'] = 'User Mass Register';
+$lang['%d email addresses already exist: %s'] = '%d e-mail-adresser är redan registrerade: %s';
+$lang['%d email addresses rejected: %s'] = '%d e-mail-adresser refuserade: %s';
+$lang['%d registrations on error: %s'] = '%d registreringar med fel: %s';
+$lang['%d users registered'] = '%d användare registrerade';
+$lang['(one on each line)'] = '(en på varje rad)';
+$lang['Email addresses'] = 'E-mail-adresser';
+$lang['Example'] = 'Exempel';
+$lang['Register users'] = 'Registrera användare';
+?>
