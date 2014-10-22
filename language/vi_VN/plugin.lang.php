1784d91bca9db825d742daa4f2849e1da2679e07
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-10-22 19:38:23 +0000

    [user_mass_register] Update vi_VN, thanks to : thanhphan467
    git-svn-id: http://piwigo.org/svn/extensions/user_mass_register@30261 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/vi_VN/plugin.lang.php
@@ -0,0 +1,31 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['User Mass Register'] = 'Đăng ký tài khoản tập thể';
+$lang['Register users'] = 'Đăng ký tài khoản';
+$lang['%d registrations on error: %s'] = '%d Lỗi khi đăng ký:%s';
+$lang['(one on each line)'] = '(một trên mỗi dòng)';
+$lang['Email addresses'] = 'Địa chỉ Email';
+$lang['Example'] = 'Ví dụ';
+$lang['%d email addresses rejected: %s'] = '%d Địa chỉ email này đã bị từ chối: %s';
+$lang['%d users registered'] = '%d Tài khoản này đã được đăng ký';
+$lang['%d email addresses already exist: %s'] = '%d Địa chỉ email này đã tồn tại: %s';
