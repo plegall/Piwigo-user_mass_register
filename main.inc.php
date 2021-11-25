38ab765b13f622a9aafb395cb4c4ec87bcdb143f
plegall <plg@piwigo.org>
2021-11-25 15:48:47 +0100

    compatibility Piwigo 12

--- main.inc.php
+++ main.inc.php
@@ -6,6 +6,7 @@ Description: Register several users at once
 Plugin URI: auto
 Author: plg
 Author URI: http://le-gall.net/pierrick
+Has Settings: true
 */
 
 defined('PHPWG_ROOT_PATH') or die('Hacking attempt!');
@@ -23,23 +24,4 @@ define('UMR_ADMIN',   get_root_url() . 'admin.php?page=plugin-' . UMR_ID);
 // +-----------------------------------------------------------------------+
 // | Add event handlers                                                    |
 // +-----------------------------------------------------------------------+
-
-// admin plugins menu link
-add_event_handler('get_admin_plugin_menu_links', 'umr_admin_plugin_menu_links');
-
-/**
- * admin plugins menu link
- */
-function umr_admin_plugin_menu_links($menu) 
-{
-  array_push(
-    $menu,
-    array(
-      'NAME' => 'User Mass Register',
-      'URL' => UMR_ADMIN,
-      )
-    );
-  
-  return $menu;
-}
 ?>
