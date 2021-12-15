0e4e6c187e2c3a34b71f6800baa8500628bcd235
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-12-15 20:46:32 +0100

    Update sl_SI, thanks to maxima

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Example'] = 'Primer';
+$lang['%d email addresses already exist: %s'] = '%d e-poštnih naslovov že obstaja: %s';
+$lang['%d email addresses rejected: %s'] = '%d e-poštnih naslovov zavrnjenih: %s';
+$lang['%d registrations on error: %s'] = '%d registracij ob napaki: %s';
+$lang['%d users registered'] = '%d registriranih uporabnikov';
+$lang['Register users'] = 'Registrirajte uporabnike';
+$lang['(one on each line)'] = '(ena v vsaki vrstici)';
+$lang['Email addresses'] = 'E-poštni naslovi';
+$lang['User Mass Register'] = 'Množični register uporabnikov';
