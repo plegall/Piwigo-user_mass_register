9bc3c256667bf87a8473cda3f6866e47c7da812d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-06-09 06:31:55 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['%d email addresses already exist: %s'] = '%d netföng eru þegar til: %s';
+$lang['%d email addresses rejected: %s'] = '%d netföngum hafnað: %s';
+$lang['%d registrations on error: %s'] = '%d skráningar á villu: %s';
+$lang['%d users registered'] = '%d notendur skráðir';
+$lang['(one on each line)'] = '(einn á hverri línu)';
+$lang['Email addresses'] = 'Netföng';
+$lang['Example'] = 'Dæmi';
+$lang['Register users'] = 'Skráðu notendur';
+$lang['User Mass Register'] = 'Fjöldaskráning notenda';
