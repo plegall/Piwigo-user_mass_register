d54387115662dc57224f17d779cb5a1b3adc379f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-21 22:48:39 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['%d email addresses already exist: %s'] = '%d כתובות דוא"ל כבר קיימות: %s';
+$lang['%d email addresses rejected: %s'] = '%d כתובות דוא"ל נדחו: %s';
+$lang['%d registrations on error: %s'] = '%d הרשמות בשגיאה: %s';
+$lang['%d users registered'] = '%d משתמשים נרשמו';
+$lang['(one on each line)'] = '(אחד בכל שורה)';
+$lang['Email addresses'] = 'כתובות דוא"ל';
+$lang['Example'] = 'דוגמה';
+$lang['Register users'] = 'משתמשים רשומים';
+$lang['User Mass Register'] = 'הרשמה מרובת משתמשים';
