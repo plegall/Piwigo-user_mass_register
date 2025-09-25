bc78bfd07b8080eac33e473b726c72e2c053517e
plegall <plg@piwigo.org>
2025-09-25 19:52:24 +0200

    fixes #1 force include functions_mail.inc.php for function switch_lang_to

--- admin.php
+++ admin.php
@@ -4,6 +4,8 @@ if (!defined("PHPWG_ROOT_PATH"))
   die ("Hacking attempt!");
 }
 
+include_once(PHPWG_ROOT_PATH.'include/functions_mail.inc.php');
+
 // +-----------------------------------------------------------------------+
 // | Functions                                                             |
 // +-----------------------------------------------------------------------+
