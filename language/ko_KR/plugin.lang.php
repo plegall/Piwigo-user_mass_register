6b4fa95528ade90399c3d121a2c2fce84f905a32
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-03 19:37:12 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/plugin.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['%d email addresses already exist: %s'] = '%d 개의 이메일 주소가 이미 존재합니다: %s';
+$lang['%d email addresses rejected: %s'] = '%d 개의 이메일 주소가 거부되었습니다: %s';
+$lang['%d registrations on error: %s'] = '%d 개의 등록이 오류 발생: %s';
+$lang['%d users registered'] = '%d 명의 사용자가 등록되었습니다';
+$lang['(one on each line)'] = '(각 줄마다 하나씩)';
+$lang['Email addresses'] = '이메일 주소';
+$lang['Example'] = '예시';
+$lang['Register users'] = '사용자 등록';
+$lang['User Mass Register'] = '사용자 일괄 등록';
